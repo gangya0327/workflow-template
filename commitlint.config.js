4790d485e0a5c08565eebfbba5d74c8db534295e
yingml <yingml@honlife.com.cn>
2022-08-19 20:17:36 +0800

    🔧工具(javascript): 添加自定义提交信息

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,4 @@
-module.exports = { extends: ['@commitlint/config-conventional'] }
+// module.exports = { extends: ['@commitlint/config-conventional'] }
+
+// 自定义规范
+module.exports = { extends: ['cz'] }
